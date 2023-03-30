30d656be47e7fafc46692b1c1d5c27c40b239c6b
chino702 <chinox3702@gmail.com>
2023-03-29 23:51:24 -0700

    the DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.setAttribute("id", "victory");
+
+expect(newHeader.getAttribute("id")).to.equal("victory");
